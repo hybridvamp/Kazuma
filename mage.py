0ff746fba35446d897c686251b217690f4857847
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-10-06 01:48:04 +0400

    C

--- mage.py
+++ mage.py
@@ -445,8 +445,8 @@ if __name__ == "__main__":
         #with open('config.json', 'r') as f: config = json.load(f)
         #sudoList = config['sudoList']
         #botToken = config['botToken']
-        sudoList = config.SUDOLIST
-        botToken = config.BOT_TOKEN
+        sudoList = Config.SUDOLIST
+        botToken = Config.BOT_TOKEN
     except:
         config ={"database": "database-name.db", "botToken": "bot-token-here", "sudoList": [12345678, 1412909688]}
         with open('config.json', 'w') as f: json.dump(config, f, indent=4)
