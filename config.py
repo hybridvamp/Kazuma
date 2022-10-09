c2f95d0e2f50e10c9b891fde61f3a6f600d4c403
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-10-09 22:47:30 +0400

    config

--- config.py
+++ config.py
@@ -6,9 +6,11 @@ load_dotenv()  # take environment variables from .env
 
 
 class Config:
+    API_ID = int(os.environ.get("API_ID", 1234))
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SUDOLIST = os.environ.get("SUDOLIST", "")
-    DATABASE = os.environ.get("DATABASE")
+    SUDOLIST = int(os.environ.get("SUDOLIST", 1412909688))
+    DATABASE = os.environ.get("DATABASE", "")
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
